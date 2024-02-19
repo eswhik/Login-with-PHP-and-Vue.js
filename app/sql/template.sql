0f1516e20d92b0e6a6fe58a8e41854d908677013
José Caruajulca <159473753+TheEswhik@users.noreply.github.com>
2024-02-18 22:53:39 -0500

    Update template.sql

--- app/sql/template.sql
+++ app/sql/template.sql
@@ -1,12 +1,13 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  username varchar(255) NOT NULL,
-  name varchar(255) NOT NULL,
-  last_name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  image varchar(255) DEFAULT NULL,
-  status varchar(50) DEFAULT 'activo',
-  creation_date timestamp NOT NULL DEFAULT current_timestamp(),
-  update_date timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-);
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `status` varchar(50) DEFAULT 'activo',
+  `creation_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
